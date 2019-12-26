878542718b9fcbef292b5d33faa76b4ca88a198f
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-26 10:06:22 +0100

    init class Car like model to create car

---
+++ src/com/cursor/exeption/model/Car.java
@@ -0,0 +1,36 @@
+package com.cursor.exeption.model;
+
+import com.cursor.exeption.driving.CarNotReadyToDriveException;
+
+public class Car {
+
+    private boolean isGasolineInsideOFTank = false;
+    private final String MODEL;
+
+    public Car(String model) {
+        this.MODEL = model;
+    }
+
+    public void drive() throws CarNotReadyToDriveException {
+        System.out.println("Trying to drive the car");
+        if (isGasolineInsideOFTank) {
+            System.out.println("You ara driving the car");
+        } else {
+            throw new CarNotReadyToDriveException("The car " +
+                    MODEL + " can not move, because tank is empty");
+        }
+    }
+
+    public void refuel() {
+        if (isGasolineInsideOFTank) {
+            System.out.println("Car is refueled");
+        } else {
+            System.out.println("Car has been refueled");
+            this.isGasolineInsideOFTank = true;
+        }
+    }
+
+    public boolean isGasolineInsideOFTank() {
+        return isGasolineInsideOFTank;
+    }
+}
