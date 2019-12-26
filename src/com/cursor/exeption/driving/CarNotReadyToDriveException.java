77c7be7170ac4ee69ed80cbb12d918c4092c233d
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-26 10:04:32 +0100

    created own exception CarNotReadyToDriveException

---
+++ src/com/cursor/exeption/driving/CarNotReadyToDriveException.java
@@ -0,0 +1,8 @@
+package com.cursor.exeption.driving;
+
+public class CarNotReadyToDriveException extends NotReadyToDriveException {
+
+    public CarNotReadyToDriveException(String message) {
+        super(message);
+    }
+}
