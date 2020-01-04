f075666f6d181dc3fecb9ef222d535a2ff112555
Andrii Dovbenko <andriydovbenko1@gmail.com>
2020-01-04 13:28:39 +0100

    renamed method

--- src/com/cursor/exeption/Main.java
+++ src/com/cursor/exeption/Main.java
@@ -3,6 +3,6 @@ package com.cursor.exeption;
 public class Main {
 
     public static void main(String[] args) {
-	new Executor().checkExampleOfExceptions();
+        new Executor().executeProject();
     }
 }
