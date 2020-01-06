1fd4034212ccd5cf8d4e4e62162b1a1ba8ccd23a
Andrii Dovbenko <andriydovbenko1@gmail.com>
2020-01-06 12:33:31 +0100

    fixed input bug

--- src/com/cursor/exeption/division/SafeDivision.java
+++ src/com/cursor/exeption/division/SafeDivision.java
@@ -5,21 +5,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SafeDivision {
+    private static final String FIRST_MASSAGE = "first";
+    private static final String SECOND_MASSAGE = "second";
     private Integer firstNumber;
     private Integer secondNumber;
+    private Scanner input = new Scanner(System.in);
 
     public SafeDivision() {
-        this.firstNumber = inputInteger(" first");
-        this.secondNumber = inputInteger(" second");
+        this.firstNumber = inputInteger(FIRST_MASSAGE);
+        this.secondNumber = inputInteger(SECOND_MASSAGE);
+        input.close();
     }
 
-    public Integer inputInteger(String number) {
-        System.out.println("Enter" + number + " integer: ");
+    public Integer inputInteger(String massage) {
+        System.out.println("Enter " + massage + " integer: ");
         Integer integerNumber = null;
         while (integerNumber == null) {
-            try (Scanner input = new Scanner(System.in)){
+            try {
                 integerNumber = input.nextInt();
             } catch (InputMismatchException e) {
+                input.nextLine();
                 System.out.println("Input error. Enter only digits");
             }
         }
